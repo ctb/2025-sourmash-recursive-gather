232d74d1b4e575468502708d81c0f339ae532aae
C. Titus Brown <titus@idyll.org>
2025-01-03 14:18:59 -0400

    update for revised api

--- run-gather.py
+++ run-gather.py
@@ -46,17 +46,21 @@ class Database:
         return f"Database({self.shortname}, {self.dbtype}://{self.path}"
 
 
-    def gather(self, query_obj, threshold_hashes, scaled, prefix):
+    def gather(self, query_obj, threshold_hashes, scaled, csvfile):
         print(f'running gather against {self.shortname}')
         start = time.time()
         assert self.handle, "must call load first"
+
         if self.dbtype == 'rocksdb':
             self.handle.fastmultigather_against(query_obj,
                                                 self.handle.selection(),
                                                 threshold_hashes,
-                                                f'{prefix}.gather.csv')
+                                                csvfile)
         elif self.dbtype == 'collection':
-            self.handle.fastmultigather_against(query_obj, threshold_hashes, scaled)
+            self.handle.fastmultigather_against(query_obj,
+                                                threshold_hashes,
+                                                scaled,
+                                                csvfile)
                                                 
         else:
             raise Exception("unknown dbtype in gather")
@@ -102,8 +106,9 @@ def main():
     against_db.load(ksize, scaled, moltype)
 
     prefix = args.query.split('.')[0]
-    against_db.gather(query_obj, 0, scaled, prefix)
-    assert os.path.exists(f'{prefix}.gather.csv')
+    outfile = f'{prefix}.gather.csv'
+    against_db.gather(query_obj, 0, scaled, outfile)
+    assert os.path.exists(outfile)
 
 
 if __name__ == '__main__':
